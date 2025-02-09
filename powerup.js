e0a1b9d2340c12277b69b46db644186bd064f4d0
kesatasarim <149082375+kesatasarim@users.noreply.github.com>
2025-02-09 14:14:59 +0300

    Update powerup.js

--- powerup.js
+++ powerup.js
@@ -1,14 +1,15 @@
 /* Trello Power-Up Başlatma */
 window.TrelloPowerUp.initialize({
-  "card-buttons": function (t, options) {
+  "card-buttons": async function (t, options) {
     return [
       {
         icon: "https://trello-powerup-eight.vercel.app/timer.png", 
         text: "Zaman Takibi",
-        callback: function (t) {
-          return t.popup({
+        callback: async function (t) {
+          return await t.popup({
             title: "Zaman Takibi",
-            url: "https://trello-powerup-eight.vercel.app/timer.html"
+            url: "https://trello-powerup-eight.vercel.app/timer.html",
+            height: 250
           });
         }
       }
